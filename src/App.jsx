2ba8e4b13e3798079f5e540c04a7db088948d32f
millenniumSonule <anshumansonuley@gmail.com>
2024-04-21 15:13:25 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import Plp from './plp';
+import data from './data.json'
 const App = () => {
   return (
-    <div>saddasdas</div>
+      <div className="container">
+          
+          <Plp data={data} />  {/* No need for double curly braces */}
+      
+      </div>
   )
 }
 
-export default App
+export default App;
--- src/plp.jsx
+++ src/plp.jsx
@@ -1,12 +1,17 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
 
-const plp = () => {
+const Plp = ({ data }) => {  
   return (
-    <View>
-      <Text>plp</Text>
-    </View>
-  )
-}
+    <div>
+      {data.map((item, index) => (
+        <div key={index}>
+          <img src={item.image} alt={item.product_name} style={{ width: '100px', height: '100px' }} />
+          <p>{item.product_name}</p>
+          <p>{item.price}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default plp
+export default Plp;
