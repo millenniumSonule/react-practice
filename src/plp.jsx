2603729772424f0bea3695a3c0a8dd6327a2c362
millenniumSonule <anshumansonuley@gmail.com>
2024-04-21 13:52:56 +0530

    .

---
+++ src/plp.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const plp = () => {
+  return (
+    <View>
+      <Text>plp</Text>
+    </View>
+  )
+}
+
+export default plp
